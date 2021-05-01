6c68bfd744390a27b4146ba43936f63e996fb484
unknown <dimas12triandi13@gmail.com>
2021-05-01 21:51:14 +0700

    login dan regis

---
+++ resources/views/login.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assetLoginRegis/css/bootstrap.css">
+    <link rel="stylesheet" href="assetLoginRegis/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assetLoginRegis/css/bootstrap.css.map">
+    <link rel="stylesheet" href="assetLoginRegis/css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="assetLoginRegis/css/style.css">
+    <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <title>Log In</title>
+  </head>
+  <body class="bd">
+    <div class="hias"></div>
+    <div class="container">
+      <div class="login-box shadow-lg">
+        <form>
+          <div class="form">
+            <h1 class="h1 mb-3 font-monospace">Log In</h1>
+            <label class="form-label mt-4">E-mail</label>
+            <input type="email" class="form-control" required autofocus>
+            <label for="" class="form-label mt-2">Password</label>
+            <input type="password" class="form-control">
+            <div class="mt-2">
+              <div class="row">
+                <div class="col-lg-6" style="text-align: left;">
+                  <input type="checkbox" value="Remember"> Remember me
+                </div>
+                <div class="col-lg-6" style="text-align: right; ">
+                  <a class="mt-0 w" href="#">Forget Password ?</a>
+                </div>
+              </div>
+            </div>
+            <button class=" text-light form-control mt-3" style="background-color: #643FDB;">Log In</button>
+            <div class="row mt-1">
+                <div class="col-lg-5"><hr></div>
+                <div class="col-lg-2 text-center">
+                  <p>or</p>
+                </div>
+                <div class="col-lg-5"><hr></div>
+            </div>
+            
+            <button class="shadow-sm form-control mt-2"><img src="assetLoginRegis/icon/google.svg" alt="" height="20px"> Log In With Google</button>
+            <hr>
+            <div class="text-center">
+              <label>Don't have an account? </label> <a href="#">sign up</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="hiasbawahkiri"></div>
+    <div class="hiasbawahkanan"></div>
+ 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="asset/js/bootstrap.js"></script>
+    <script src="asset/js/bootstrap.min.js"></script>
+  </body>
+</html>
